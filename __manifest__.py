c9a2fcc719c87ae65f6fcaa6a550c09a5fc838e7
Khanstore <dmashraf@gmail.com>
2026-05-02 01:35:30 +0600

    licence and version added

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Advanced Business Dashboard",
-    "version": "18.0.0.1",
+    "version": "18.0.0.2",
     "author": "SM Ashraf",
     "depends": ["sale", "purchase", "account", "web"],
     "data": ["views/menu.xml"],
@@ -8,10 +8,12 @@
         "web.assets_backend": [
             "eagle_business_dashboard/static/src/js/dashboard.js",
             "eagle_business_dashboard/static/src/xml/dashboard.xml",
-            "eagle_business_dashboard/static/src/css/dashboard.css"
+            "eagle_business_dashboard/static/src/css/dashboard.css",
+            "eagle_business_dashboard/static/src/js/finance_dashboard.js",
+            "eagle_business_dashboard/static/src/xml/finance_dashboard.xml",
         ]
     },
     "installable": True,
-    "licence": 'LGPL-3' ,
+    "licence": "LGPL-3",
     "application": True
 }
